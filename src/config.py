9e1c7d655e88e176e34b795244ff3df5d2f215ad
Simrit Nijjar <simritnijjar@outlook.com>
2025-06-23 22:54:29 -0700

    updated params to match account schema

--- src/config.py
+++ src/config.py
@@ -1,11 +1,14 @@
 import json
 import os
-from typing import TypedDict
+from typing import TypedDict, List, Dict, Any
+import lib.definitions as d
 
 class Params(TypedDict):
     STATEMENT_FOLDER: str
+    VANCITY_PATH: str
     SUPPORTED_EXTENSIONS: list[str]
     DEBUG_MODE: bool
+    LOANS: List[Dict[str, Any]]
 
 # Load params.json from the project root
 root_dir = os.path.dirname(os.path.abspath(__file__))
